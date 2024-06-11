bdd27a852456608aab646d65f6f9fb4f91851767
sky <sky@153.com>
2024-06-11 16:41:48 +0800

    小小 的提交 #skip

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -58,7 +58,7 @@ const PokemonPage = ({ previous, next }) => {
                     <td className="pokemon-stats-value">{value}</td>
                   </tr>
                 ))}
-                种族值是
+                他的种族值
                 {stats.reduce((base, cur) => {
                   return base + cur.value;
                 }, 0)}
